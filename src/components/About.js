445dd2eb05cc37b6ba0a936aa608d60a98948991
augustioo <aaoguamanam@uh.edu>
2020-07-14 16:19:48 -0500

    Made changes and added files

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid consequuntur error fugit illo natus nobis quae quibusdam repellat sapiente sed suscipit tempore, temporibus. Dolor eum nesciunt pariatur perferendis quia.</p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid consequuntur error fugit illo natus nobis quae quibusdam repellat sapiente sed suscipit tempore, temporibus. Dolor eum nesciunt pariatur perferendis quia.</p>
+        </div>
+    )
+}
+
+export default Home
